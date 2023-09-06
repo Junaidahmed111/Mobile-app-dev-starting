33c114088864fbbc91ee152a581f1a4d86205843
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-09-06 22:36:16 +0500

    flex in react native

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
-  return <View style={styles.container}>
-<Text>hello world</Text>
-
-
-  </View>;
+  return (
+    <View style={styles.appConatiner}>
+      {/* input area */}
+      <View>
+        <TextInput placeholder="your course goal" />
+        <Button title="add goal" />
+      </View>
+      {/* lists of goals rendered */}
+      <View>
+        <Text>List of goals</Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+  appConatiner: {
+    padding: 50,
   },
 });
