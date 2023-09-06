cceb79a771f3a0937d5bcfa414ce37e53b609006
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-09-06 22:03:31 +0500

    hello world

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start work on your app!</Text>
-      <Button title="Clicm me!"></Button>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <View style={styles.container}>
+<Text>hello world</Text>
+
+
+  </View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "purple",
     alignItems: "center",
     justifyContent: "center",
   },
