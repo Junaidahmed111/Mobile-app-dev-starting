f3ac4e561063a9976849f230eae7a8d97f632ab9
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-09-18 21:02:05 +0500

    fixing

--- App.js
+++ App.js
@@ -38,6 +38,7 @@ export default function App() {
           color="red"
           onPress={startaddGoalHandler}
         />
+        {/* input area */}
         <GoalInput
           visible={modalIsVisible}
           onAddGoal={addGoalHandler}
